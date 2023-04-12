6e9b269a6f8f87cf61847de79de828a858602bf9
pain <legendofpain5112@gmail.com>
2023-04-12 14:28:46 +0500

    add arrow control

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,13 +9,15 @@ const audio3 = audi[2]
 const audio4 = audi[3]
 
 const sounds = [audio1, audio2, audio3, audio4]
-console.log(sounds)
 function initSoundOnHover() {
   answerButtons.forEach((item, index) => {
     item.addEventListener('mouseover', () => {
       sounds[index].play()
     })
     item.addEventListener('click', () => {
+      sounds[index].play()
+    })
+    item.addEventListener('focus', (e) => {
       sounds[index].play()
     })
   })
@@ -37,4 +39,69 @@ function onCLickVolumeIcon() {
 volumeBtn.addEventListener('click', () => {
   volumeBtn.classList.toggle('volume-on')
   onCLickVolumeIcon()
+})
+
+//
+const quiz1 = answerButtons[0]
+const quiz2 = answerButtons[1]
+const quiz3 = answerButtons[2]
+const quiz4 = answerButtons[3]
+
+window.addEventListener('keydown', (e) => {
+  let focusIndex = e.target.dataset.focus
+
+  switch (e.key) {
+    case 'ArrowLeft':
+      if (focusIndex === '1') {
+        quiz1.focus()
+      }
+      if (focusIndex === '2') {
+        quiz2.focus()
+      }
+      if (focusIndex === '3') {
+        quiz3.focus()
+      }
+      if (focusIndex === '4') {
+        quiz4.focus()
+      }
+      break
+    case 'ArrowRight':
+      if (focusIndex === '0') {
+        quiz2.focus()
+      }
+      if (focusIndex === '1') {
+        quiz3.focus()
+      }
+      if (focusIndex === '2') {
+        quiz4.focus()
+      }
+      break
+    case 'ArrowUp':
+      if (focusIndex === '1') {
+        quiz1.focus()
+      }
+      if (focusIndex === '2') {
+        quiz2.focus()
+      }
+      if (focusIndex === '3') {
+        quiz3.focus()
+      }
+      if (focusIndex === '4') {
+        quiz4.focus()
+      }
+
+      break
+    case 'ArrowDown':
+      if (focusIndex === '0') {
+        quiz2.focus()
+      }
+      if (focusIndex === '1') {
+        quiz3.focus()
+      }
+      if (focusIndex === '2') {
+        quiz4.focus()
+      }
+
+      break
+  }
 })
